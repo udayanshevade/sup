0f15d3954356816441d5a3c836fd69c7c29206c3
Udayan Shevade <udayan.shevade@gmail.com>
2020-11-17 00:15:28 -0800

    add video component tests

--- src/components/Video/__tests__/Video.spec.js
+++ src/components/Video/__tests__/Video.spec.js
@@ -1,18 +1,38 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { Video } from '../';
 
 describe('Video', () => {
+  const defaultProps = {
+    id: 'foo',
+    className: 'foo',
+    stream: {},
+    autoPlay: true,
+    playsInline: true,
+    controls: true,
+    getVideoRef: jest.fn(),
+  };
+
   it('should render', () => {
-    const defaultProps = {
-      id: 'foo',
-      className: 'foo',
-      stream: {},
-      autoPlay: true,
-      playsInline: true,
-      controls: false,
-      getVideoRef: jest.fn(),
-    };
     render(<Video {...defaultProps} />);
-    expect(screen.getByTestId('video-element')).toBeInTheDocument();
+    const video = screen.getByTestId('video-element');
+    expect(video).toBeInTheDocument();
+    expect(video).toHaveAttribute('id', defaultProps.id);
+    expect(video).toHaveClass(defaultProps.className);
+    expect(video).toHaveAttribute('autoPlay');
+    expect(video).toHaveAttribute('playsInline');
+    expect(video).toHaveAttribute('controls');
+  });
+
+  it('should set the srcObject', async () => {
+    render(<Video {...defaultProps} />);
+    const video = screen.getByTestId('video-element');
+    await waitFor(() =>
+      expect(video).toHaveProperty('srcObject', defaultProps.stream)
+    );
+  });
+
+  it('expect passed down video ref callback to be called', () => {
+    render(<Video {...defaultProps} />);
+    expect(defaultProps.getVideoRef).toHaveBeenCalled();
   });
 });
